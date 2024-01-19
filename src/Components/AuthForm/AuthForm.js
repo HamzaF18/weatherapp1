df1b041235d5203e292e7f0910f6109c3bb8ee09
HamzaF18 <40198734@hruc.ac.uk>
2024-01-19 09:58:33 +0000

    init

--- src/Components/AuthForm/AuthForm.js
+++ src/Components/AuthForm/AuthForm.js
@@ -2,7 +2,15 @@ import "./AuthForm.css";
 
 const AuthForm = ()=>{
     return(
-        <div></div>
+        <div className="auth-container">
+
+            <form className="auth-form">
+                <h1>Sign in</h1>
+                <input className="auth-login" type="text" placeholder="username"/>
+                <input className="auth-login" type="password" placeholder="password"/>
+            </form>
+            
+        </div>
     )
 }
 export default AuthForm;
