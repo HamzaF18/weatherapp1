c1a3ad3df60382b930c5d65d3f8871e71e40e6e6
HamzaF18 <40198734@hruc.ac.uk>
2024-01-19 12:43:22 +0000

    init

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import {Route, Routes} from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
 import WeatherCondition from './Components/WeatherCondition/WeatherCondition';
 import AuthForm from './Components/AuthForm/AuthForm';
+import Home from "./Components/Home/Home";
+
 
 function App() {
 
+
   return (
+    
     <div className="App">
       <NavBar/>
 
 
-
-
       <Routes>
-        
         <Route path="/weather" element={<WeatherCondition/>}/>
         <Route path="/login" element={<AuthForm/>}/>
       </Routes>
       
+
+     
     </div>
+    
+    
   );
 }
 
