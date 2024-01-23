28ec0b2cf7eb10f78cb01d90abf80c78ba2d298b
HamzaF18 <40198734@hruc.ac.uk>
2024-01-23 15:47:52 +0000

    macej feedback

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,14 @@
 import "./Home.css"
 
+//make a component called day card
+//make it accept props for the things you want to display
+
 const Home = ()=>{
+    
+    function makeWeatherDay(day){
+        <span>average temp {day.average}</span>
+    }
+    
     return(
         <div className="home-container">
             <div className="top">
@@ -8,7 +16,19 @@ const Home = ()=>{
                     <input className="location" name="text" placeholder="Search Location..."></input>
                 </div>
                 <div className="cards">
-                    <div className="card"><p>Monday 20 C</p></div>
+
+                    {/* 
+                        once you have the data
+                        map over it and generate the cards one by one by giving them the data
+
+                        TheListOfDaysYouGotFromAPI.map(makeWeatherDay)
+                    */}
+                    <div className="card">
+                        <span>Monday</span>
+                        <span>Average Temp 20C</span>
+                        <span>L</span>
+                        <span>H</span>
+                    </div>
                     <div className="card"><p>Tuesday 19 C</p></div>
                     <div className="card"><p>Wednesday 16 C</p></div>
                     <div className="card"><p>Thurseday 12 C</p></div>
