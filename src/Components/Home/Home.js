02e8fced89d6d16ee3e0c1f23819b324ea7b0a97
HamzaF18 <40198734@hruc.ac.uk>
2024-01-23 09:58:00 +0000

    init

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,20 @@ import "./Home.css"
 
 const Home = ()=>{
     return(
-        <div>This is the home page</div>
+        <div className="home-container">
+            <div className="top">
+                <div className="search">
+                    <input className="location" name="text" placeholder="Search Location..."></input>
+                </div>
+                <div className="cards">
+                    <span>HI</span>
+                    <span>hI</span>
+                    <span>hi</span>
+                    <span>hi</span>
+                    <span>hi</span>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Home;
