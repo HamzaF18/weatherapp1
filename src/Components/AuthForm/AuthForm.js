3171a7be08f8fc4efe29a3c2be384e870bfa655b
HamzaF18 <40198734@hruc.ac.uk>
2024-01-26 16:42:02 +0000

    init

--- src/Components/AuthForm/AuthForm.js
+++ src/Components/AuthForm/AuthForm.js
@@ -24,7 +24,7 @@ const AuthForm = () => {
                 <div className="auth-content">
                     <h1>Sign in</h1>
                     <input className="auth-login" type="text" placeholder="username" />
-                    <input className="auth-login" type="password" placeholder="password" />
+                    <input className="auth-login" type="password" placeholder="password"/>
                     <div className="auth-button">
                         <button id="button1">Submit</button>
                     </div>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,25 +11,28 @@ const Home = () => {
 
     const [weatherData, setWeatherData] = useState();
     const [city, setCity] = useState();
+
     const api_key = "1585b602fad78a768ac1f449afa04da6";
 
-    const getWeather = (event) =>{
-        if (event.key === "Enter"){
+    const getWeather = (event) => {
+        if (event.key === "Enter") {
             fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`).then(response => response.json()
             ).then(
-                data=>{
+                data => {
                     setWeatherData(data)
                 }
             )
         }
     }
-   
+
 
     // function makeWeatherDay(day) {
     // <span>average temp {day.average}</span>
     // }
 
     // const API_KEY = fetch("https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=c60a15bb68edd82da3a14067a230b570")
+
+
 
 
     return (
@@ -43,45 +46,53 @@ const Home = () => {
                         value={city}
                         onKeyPress={getWeather}></input>
                 </div>
-                <div className="cards">
 
-                    {/* 
-                        once you have the data
-                        map over it and generate the cards one by one by giving them the data
 
-                        TheListOfDaysYouGotFromAPI.map(makeWeatherDay)
-                    */}
-                    <div className="card">
-                        <span>Monday</span>
-                        <span>Average Temp 20C</span>
-                        <span>L</span>
-                        <span>H</span>
+                {typeof weatherData?.main === 'undefined' ? (
+                    <div></div>
+                ) : (
+
+
+                    <div className="cards">
+                        
+
+                        <div className="card">
+                            {/* <span>{Date(weatherData.main.dt*1000)}</span> */}
+                            <span>Average Temp {weatherData.main.temp}</span>
+                        </div>
+
+                        <div className="card">
+                            <span>Monday</span>
+                            <span>Average Temp 20C</span>
+                            <span>L</span>
+                            <span>H</span>
+                        </div>
+                        <div className="card">
+                            <span>Tuesday</span>
+                            <span>Average Temp 10C</span>
+                            <span>H</span>
+                            <span>H</span>
+                        </div>
+                        <div className="card">
+                            <span>Wednesday</span>
+                            <span>Average Temp 18C</span>
+                            <span>L</span>
+                            <span>H</span>
+                        </div>
+                        <div className="card">
+                            <span>Thurseday</span>
+                            <span>Average Temp 14C</span>
+                            <span>L</span>
+                            <span>L</span>
+                        </div>
+                        <div className="card">
+                            <span>Friday</span>
+                            <span>Average Temp 15C</span>
+                            <span>H</span>
+                            <span>L</span>
+                        </div>
                     </div>
-                    <div className="card">
-                        <span>Tuesday</span>
-                        <span>Average Temp 10C</span>
-                        <span>H</span>
-                        <span>H</span>
-                    </div>
-                    <div className="card">
-                        <span>Wednesday</span>
-                        <span>Average Temp 18C</span>
-                        <span>L</span>
-                        <span>H</span>
-                    </div>
-                    <div className="card">
-                        <span>Thurseday</span>
-                        <span>Average Temp 14C</span>
-                        <span>L</span>
-                        <span>L</span>
-                    </div>
-                    <div className="card">
-                        <span>Friday</span>
-                        <span>Average Temp 15C</span>
-                        <span>H</span>
-                        <span>L</span>
-                    </div>
-                </div>
+                )}
             </div>
             <div className="bottom">
                 <div className="map"></div>
